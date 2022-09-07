aa175016dd4f4236f838d644854963b4257c03ca
Lewis Lakerink <llakerink@swin.edu.au>
2022-09-08 08:35:52 +1000

    Remove upstream websocket server library

---
+++ src/Core/MessageHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lewis on 9/8/22.
+//
+
+#include "MessageHandler.h"
---
+++ src/Core/MessageHandler.h
@@ -0,0 +1,12 @@
+//
+// Created by lewis on 9/8/22.
+//
+
+#ifndef ADACS_JOB_CLIENT_MESSAGEHANDLER_H
+#define ADACS_JOB_CLIENT_MESSAGEHANDLER_H
+
+
+void handleMessage(Message);
+
+
+#endif //ADACS_JOB_CLIENT_MESSAGEHANDLER_H
