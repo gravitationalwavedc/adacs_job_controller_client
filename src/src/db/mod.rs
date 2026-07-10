097d012290daca4e1eefd1237cf14702e194c408
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-10 23:56:16 +1000

    chore(gardener): add get_job_by_id websocket error test

--- src/src/db/mod.rs
+++ src/src/db/mod.rs
@@ -490,54 +490,23 @@ mod tests {
     }
 
     #[test]
-    fn parse_job_maps_zero_optional_ids_to_none() {
-        let mut msg = Message::new(DB_RESPONSE, Priority::Highest, "database");
-        msg.push_ulong(11);
-        msg.push_ulong(0);
-        msg.push_ulong(0);
-        msg.push_bool(false);
-        msg.push_uint(0);
-        msg.push_string("bundle-hash");
-        msg.push_string("/tmp/workdir");
-        msg.push_bool(false);
-        msg.push_bool(false);
-        msg.push_bool(false);
+    fn get_job_by_id_returns_error_when_websocket_send_fails() {
+        let _guard = TEST_MUTEX.lock().unwrap();
+        reset_websocket_client_for_test();
+        let mut mock = MockWebsocketClient::new();
+        mock.expect_send_db_request().times(1).returning(|_| {
+            Box::pin(async move {
+                Err::<Message, Box<dyn std::error::Error + Send + Sync>>("connection reset".into())
+            })
+        });
+        set_websocket_client(Arc::new(mock));
 
-        let mut resp = Message::from_data(msg.get_data().clone());
-        let model = parse_job(&mut resp);
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let result = rt.block_on(async { get_job_by_id(42).await });
 
-        assert_eq!(model.id, 11);
-        assert_eq!(model.job_id, None);
-        assert_eq!(model.scheduler_id, None);
-        assert_eq!(model.bundle_hash, "bundle-hash");
-        assert_eq!(model.working_directory, "/tmp/workdir");
-    }
-
-    #[test]
-    fn parse_job_reads_boolean_flags_including_deleted() {
-        let mut msg = Message::new(DB_RESPONSE, Priority::Highest, "database");
-        msg.push_ulong(11);
-        msg.push_ulong(22);
-        msg.push_ulong(33);
-        msg.push_bool(true);
-        msg.push_uint(4);
-        msg.push_string("bundle-hash");
-        msg.push_string("/tmp/workdir");
-        msg.push_bool(true);
-        msg.push_bool(true);
-        msg.push_bool(true);
-
-        let mut resp = Message::from_data(msg.get_data().clone());
-        let model = parse_job(&mut resp);
-
-        assert_eq!(model.id, 11);
-        assert_eq!(model.job_id, Some(22));
-        assert_eq!(model.scheduler_id, Some(33));
-        assert!(model.submitting);
-        assert_eq!(model.submitting_count, 4);
-        assert!(model.running);
-        assert!(model.deleting);
-        assert!(model.deleted);
+        assert!(result.is_err());
+        let err = result.err().unwrap();
+        assert!(err.contains("connection reset"));
     }
 
     #[test]
