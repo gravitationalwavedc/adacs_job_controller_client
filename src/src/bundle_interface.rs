95d1e006b2181a9b72795a234d4fb645657dc09b
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-26 12:58:30 +1000

    chore(gardener): Fix bundle module reference leak in BundleInterface::new error path

--- src/src/bundle_interface.rs
+++ src/src/bundle_interface.rs
@@ -251,6 +251,7 @@ impl BundleInterface {
             Py_DecRef(p_global);
             Py_DecRef(json_module);
             Py_DecRef(traceback_module);
+            Py_XDECREF(p_bundle_module);
             return Err("Failed to load bundle module".to_string());
         }
 
